0e0f9b298a1480296f9d270c605811cbfd95f867
Laurensius Adi <laurensiusadi@gmail.com>
2018-12-15 17:37:29 +0700

    Add menu bubble and floating menu, config runtime compiler

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,55 +1,71 @@
 <template>
-	<div>
-		<editor-menu-bar :editor="editor">
+	<div class="editor">
+    <editor-menu-bubble :editor="editor">
       <div
-        class="menubar is-hidden"
-        :class="{ 'is-focused': focused }"
-        slot-scope="{ commands, isActive, focused }"
+        slot-scope="{ commands, isActive, menu }"
+        class="menububble"
+        :class="{ 'is-active': menu.isActive }"
+        :style="`left: ${menu.left}px; bottom: ${menu.bottom}px;`"
       >
-				<button
-          class="menubar__button"
+        <button
+          class="menububble__button"
           :class="{ 'is-active': isActive.bold() }"
           @click="commands.bold"
+          title="Bold"
         >
           <icon name="bold" />
         </button>
 
         <button
-          class="menubar__button"
+          class="menububble__button"
           :class="{ 'is-active': isActive.italic() }"
           @click="commands.italic"
+          title="Italic"
         >
           <icon name="italic" />
         </button>
 
-				<button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.code() }"
-          @click="commands.code"
-        >
-          <icon name="code" />
-        </button>
-
         <button
-          class="menubar__button"
+          class="menububble__button"
           :class="{ 'is-active': isActive.underline() }"
           @click="commands.underline"
+          title="Underline"
         >
           <icon name="underline" />
         </button>
 
         <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.paragraph() }"
-          @click="commands.paragraph"
+          class="menububble__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+          title="Strikethrough"
         >
-          <icon name="paragraph" />
+          <icon name="strike" />
         </button>
 
+        <button
+          class="menububble__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+          title="Inline Code"
+        >
+          <icon name="code" />
+        </button>
+
+      </div>
+    </editor-menu-bubble>
+		<editor-floating-menu :editor="editor">
+      <div
+        slot-scope="{ commands, isActive, menu }"
+        class="editor__floating-menu"
+        :class="{ 'is-active': menu.isActive }"
+        :style="`top: ${menu.top}px`"
+      >
         <button
           class="menubar__button"
           :class="{ 'is-active': isActive.heading({ level: 1 }) }"
           @click="commands.heading({ level: 1 })"
+          title="Heading Large"
         >
           H1
         </button>
@@ -58,6 +74,7 @@
           class="menubar__button"
           :class="{ 'is-active': isActive.heading({ level: 2 }) }"
           @click="commands.heading({ level: 2 })"
+          title="Heading Medium"
         >
           H2
         </button>
@@ -66,6 +83,7 @@
           class="menubar__button"
           :class="{ 'is-active': isActive.heading({ level: 3 }) }"
           @click="commands.heading({ level: 3 })"
+          title="Heading Small"
         >
           H3
         </button>
@@ -74,6 +92,7 @@
           class="menubar__button"
           :class="{ 'is-active': isActive.bullet_list() }"
           @click="commands.bullet_list"
+          title="Bullet List"
         >
           <icon name="ul" />
         </button>
@@ -82,35 +101,38 @@
           class="menubar__button"
           :class="{ 'is-active': isActive.ordered_list() }"
           @click="commands.ordered_list"
+          title="Number List"
         >
           <icon name="ol" />
+        </button>
+        
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.todo_list() }"
+          @click="commands.todo_list"
+          title="Todo List"
+        >
+          <icon name="checklist" />
         </button>
 
 				<button
           class="menubar__button"
           :class="{ 'is-active': isActive.code_block() }"
           @click="commands.code_block"
+          title="Code Block"
         >
 					<icon name="code" />
 				</button>
 
-        <button
-          class="menubar__button"
-          :class="{ 'is-active': isActive.todo_list() }"
-          @click="commands.todo_list"
-        >
-          <icon name="checklist" />
-        </button>
-
       </div>
-		</editor-menu-bar>
+		</editor-floating-menu>
 		<editor-content class="editor__content" :editor="editor"/>
 	</div>
 </template>
 
 <script>
 import Icon from '@/components/Icon'
-import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Editor, EditorContent, EditorFloatingMenu, EditorMenuBubble } from 'tiptap'
 import {
 	Blockquote,
 	BulletList,
@@ -121,7 +143,8 @@ import {
 	OrderedList,
 	TodoItem,
 	TodoList,
-	Bold,
+  Bold,
+  Strike,
 	Code,
 	Italic,
 	Link,
@@ -131,7 +154,11 @@ import {
 
 export default {
 	components: {
-		Editor, EditorContent, EditorMenuBar, Icon,
+    Editor,
+    EditorContent,
+    EditorFloatingMenu,
+    EditorMenuBubble,
+    Icon,
 	},
 	data() {
 		return {
@@ -152,6 +179,7 @@ export default {
 				new TodoItem(),
 				new TodoList(),
 				new Bold(),
+				new Strike(),
 				new Code(),
 				new Italic(),
 				new Link(),
@@ -187,3 +215,24 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.editor {
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 10px 15px;
+  position: relative;
+  &__floating-menu {
+    position: absolute;
+    margin-top: -0.25rem;
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0.2s, visibility 0.2s;
+    &.is-active {
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+}
+
+</style>
